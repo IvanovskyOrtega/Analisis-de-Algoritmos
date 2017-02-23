2a817344daf85e4a4b6d655b7c7d431b3fb62563
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-22 20:50:57 -0600

    metodos.h

---
+++ Metodos-de-Ordenamiento/metodos.h
@@ -0,0 +1,11 @@
+#ifndef _METODOS_H_
+#define _METODOS_H_
+
+int * burbuja(int *A, int n);
+int * burbuja_Mejorada(int *A, int n);
+int * insercion(int *A, int n);
+int * seleccion(int *A, int n);
+int * shell(int *A, int n);
+void imprimirResultados( double utime0, double stime0, double wtime0, double utime1, double stime1, double wtime1, int n );
+
+#endif
