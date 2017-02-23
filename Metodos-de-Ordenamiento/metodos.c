531e51b16a9096714ce06aca90cfe79656144010
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-23 11:32:37 -0600

    El método Shell no funciona por el momento

--- Metodos-de-Ordenamiento/metodos.c
+++ Metodos-de-Ordenamiento/metodos.c
@@ -81,12 +81,13 @@ seleccion(int *A, int n)
   {
     pos2=pos1;
     pos3=pos1+1;
-    while(pos3>n-1)
+    while(!(pos3>n-1))
     {
       if(A[pos3]<A[pos2])
       {
         pos2=pos3;
       }
+	pos3++;
     }
     if(pos2!=pos1)
     {
@@ -105,20 +106,20 @@ shell(int *A, int n)
   int pos1=0;
   int pos2=0;
   int pos3=0;
-  int pibote=0;
+  int pivote=0;
   for(pos1=n/2;pos1>=1;)
   {
     pos2=pos1;
     while(pos2>=n)
     {
-      pibote=A[pos2];
+      pivote=A[pos2];
       pos3=pos2-pos1;
-      while((pos3>=0)&&(A[pos3]>pibote))
+      while((pos3>=0)&&(A[pos3]>pivote))
       {
         A[pos3-pos1]=A[pos3];
         pos3-=pos1;
       }
-      A[pos3+pos1]=pibote;
+      A[pos3+pos1]=pivote;
     }
     pos1/=2;
   }
@@ -126,10 +127,10 @@ shell(int *A, int n)
 }
 
 void 
-imprimirResultados( double utime0, double stime0, double wtime0, double utime1, double stime1, double wtime1, int n )
+imprimirResultados( double utime0, double stime0, double wtime0, double utime1, double stime1, double wtime1, int n, int* A )
 {
 	FILE* resultados;
-	resultados = fopen("/home/ivanovsky/Algorythms-Analisis/Sort-Methods/resultados.txt", "w");
+	resultados = fopen("/home/ivanovsky/Escritorio/Analisis-de-Algoritmos/Metodos-de-Ordenamiento/resultados.txt", "w");
 	fprintf(resultados,"Ordenar %d numeros:", n);	
 	fprintf(resultados,"\n");
 	fprintf(resultados,"\tReal (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
@@ -137,5 +138,16 @@ imprimirResultados( double utime0, double stime0, double wtime0, double utime1, 
 	fprintf(resultados,"\tSys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
 	fprintf(resultados,"\tCPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
 	fprintf(resultados,"\n");
+	fprintf(resultados,"\n");
+	fprintf(resultados,"real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+	fprintf(resultados,"user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+	fprintf(resultados,"sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+	fprintf(resultados,"CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	fprintf(resultados,"\n");
+		
+	for( int i = 0 ; i < n  ; i++)
+	{
+		fprintf(resultados, "\t%d)\t%d\n",i+1,A[i]);
+	}
 	fclose(resultados);
 }
