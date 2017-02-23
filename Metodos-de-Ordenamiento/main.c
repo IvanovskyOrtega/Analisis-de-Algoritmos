d04a4202b29143c457214ebc47a28caf24f548b3
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-23 11:35:12 -0600

    Falta implementar menú de selección de método

--- Metodos-de-Ordenamiento/main.c
+++ Metodos-de-Ordenamiento/main.c
@@ -16,12 +16,12 @@ main( void )
 	int n;
 	int tam;
 	int i=0;
-	int* A;
+	int* A = NULL;
 	printf("Ingresa la cantidad de números a ordenar:");
 	scanf("%d",&n);
 	tam = n;
 	A = (int*)malloc(n*sizeof(int));
-	FILE* numeros = fopen("/home/ivanovsky/Algorythms-Analisis/Sort-Methods/numeros.txt", "rb");
+	FILE* numeros = fopen("/home/ivanovsky/Descargas/numeros10millones.txt", "r");
 	while (feof(numeros)==0 && i < n)
       	{
             fscanf(numeros,"%d",&A[i]);
@@ -29,7 +29,7 @@ main( void )
         }
 	fclose(numeros);
 	uswtime(&utime0, &stime0, &wtime0);
-	burbuja(A, n);
+	A = aBB(A, n);
 	uswtime(&utime1, &stime1, &wtime1);
-	imprimirResultados( utime0, stime0, wtime0, utime1, stime1, wtime1, tam );
+	imprimirResultados( utime0, stime0, wtime0, utime1, stime1, wtime1, tam, A );
 }
