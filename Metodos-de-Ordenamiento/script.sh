4370509b4ed02d67db58c4b2b074af8c9ee42a14
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-26 16:07:54 -0600

    Aun no se comprueba su funcionalidad

---
+++ Metodos-de-Ordenamiento/script.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Script para la práctica 1
+echo Ejecutando el programa para 1000 elementos con cada método de ordenamiento
+cd /home/ivanovsky/Escritorio/Analisis-de-Algoritmos/Metodos-de-Ordenamiento
+./a.out < entradas
+echo Terminamos uwu
+
