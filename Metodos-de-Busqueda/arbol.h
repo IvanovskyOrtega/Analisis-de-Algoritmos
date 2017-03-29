d79ee559e6e07a6fabff6c3433989f86960dbdfe
Ivanovsky <ivanovskyortega@gmail.com>
2017-03-29 13:29:21 -0600

    Librería de árbol binario de búsqueda

---
+++ Metodos-de-Busqueda/arbol.h
@@ -0,0 +1,21 @@
+#ifndef _ARBOL_H_
+#define _ARBOL_H_
+
+struct nodo
+{
+  int dato;
+  struct nodo *Izq;
+  struct nodo *Der;
+};
+
+int
+aBB (int* A, int n, int num);
+void
+insertar (struct nodo **Raiz, int miDato1);
+struct nodo *
+nuevoNodo (int dato1);
+int *
+inordenSecuencial(struct nodo *Raiz, int* A);
+int
+buscarABB(struct nodo *Raiz, int num);
+#endif
