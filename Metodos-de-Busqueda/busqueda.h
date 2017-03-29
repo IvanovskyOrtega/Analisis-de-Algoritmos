a4261bd909ceb1b945428795027e52f11faccea1
Ivanovsky <ivanovskyortega@gmail.com>
2017-03-29 13:28:34 -0600

    Librería de búsqueda

---
+++ Metodos-de-Busqueda/busqueda.h
@@ -0,0 +1,12 @@
+#ifndef _BUSQUEDA_H_
+#define _BUSQUEDA_H_
+
+void
+busqueda();
+int
+busquedaLineal( int* A, int n, int num );
+int
+busquedaBinaria( int* A, int n, int num );
+//void busquedaABB( int* A, int n );
+
+#endif
