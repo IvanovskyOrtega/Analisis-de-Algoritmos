455e13004228a2243e10017157d2ecbfbdcb7f8d
Ivanovsky <ivanovskyortega@gmail.com>
2017-04-02 17:29:21 -0500

    Modificación en la impresión de resultados

--- Metodos-de-Busqueda/busqueda.c
+++ Metodos-de-Busqueda/busqueda.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "busqueda.h"
 #include "arbol.h"
+#include "tiempo.h"
 
 void
 leerNumeros( int* A, int n )
@@ -24,8 +25,14 @@ busqueda()
   int op;
   int num;
   int index;
+  double utime0;
+  double stime0;
+  double wtime0;
+  double utime1;
+  double stime1;
+  double wtime1;
   system( "clear" );
-  printf( "Vamo' a buscar :v\n" );
+  printf( "Practica 2: Algoritmos de Busqueda\n" );
   printf("\nTamaño del arreglo de busqueda:");
 	scanf( "%d" , &n );
   A = (int*)malloc(n*sizeof(int));
@@ -39,30 +46,35 @@ busqueda()
   switch( op )
   {
     case 1:
+      uswtime(&utime0, &stime0, &wtime0);
       index = busquedaLineal( A , n , num );
+      uswtime(&utime1, &stime1, &wtime1);
       if( index != -1 )
         printf("Se encontro el numero %d en el indice %d del arreglo.\n",num,index);
       else
-        printf("No esta el numero prro :'v\n'");
+        printf("No se encontro el numero.\n'");
       break;
     case 2:
+      uswtime(&utime0, &stime0, &wtime0);
       index = busquedaBinaria( A , n , num );
+      uswtime(&utime1, &stime1, &wtime1);
       if( index != -1 )
         printf("Se encontro el numero %d en el indice %d del arreglo.\n",num,index);
       else
-        printf("No esta el numero prro :'v\n'");
+        printf("No se encontro el numero.\n'");
       break;
     case 3:
       index = aBB( A, n, num );
       if( index != -1 )
         printf("Se encontro el numero %d sobre el arbol.\n",num);
       else
-        printf("No esta el numero prro :'v\n'");
+        printf("No se encontro el numero.\n'");
       break;
     default:
-      printf("Khe? :v\n");
+      printf("Opcion no valida.\n");
       break;
   }
+  imprimirResultados( utime0, stime0, wtime0, utime1, stime1, wtime1, op, num, n, index);
 }
 
 int
@@ -93,4 +105,36 @@ busquedaBinaria( int* A, int n, int num )
         inf = centro + 1;
   }
   return -1;
+}
+
+void
+imprimirResultados( double utime0, double stime0, double wtime0, double utime1, double stime1, double wtime1, int op, int num, int n, int index )
+{
+  FILE* resultados;
+  resultados = fopen("resultados.txt", "a");
+  if( op == 1 )
+    fprintf(resultados,"Busqueda con el algoritmo de busqueda lineal\n");
+  else if( op == 2 )
+    fprintf(resultados,"Busqueda con el algoritmo de busqueda binaria\n");
+  else
+    fprintf(resultados,"Busqueda con el algoritmo de busqueda sobre un ABB\n");
+  fprintf(resultados,"\n");
+  fprintf(resultados,"\tSe busco el numero: %d, sobre un arreglo de %d numeros\n",num,n);
+  if(index != -1)
+    fprintf(resultados,"\tEl numero fue encontrado.\n");
+  else
+    fprintf(resultados,"\tEl numero no fue encontrado.\n");
+  fprintf(resultados,"\tFormato exponencial:\n");
+  fprintf(resultados,"\tReal (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+  fprintf(resultados,"\tUser (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+  fprintf(resultados,"\tSys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+  fprintf(resultados,"\tCPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+  fprintf(resultados,"\n");
+  fprintf(resultados,"\n");
+  fprintf(resultados,"\tFormato segundos:\n");
+  fprintf(resultados,"\tReal (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+  fprintf(resultados,"\tUser (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+  fprintf(resultados,"\tSys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+  fprintf(resultados,"\tCPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+  fclose(resultados);
 }
