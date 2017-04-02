59a3ce6b38a875a71c87f7c3328ec3eccdcbe624
Ivanovsky <ivanovskyortega@gmail.com>
2017-04-02 16:18:00 -0500

    Ya funciona el script

--- Metodos-de-Ordenamiento/script.sh
+++ Metodos-de-Ordenamiento/script.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 # Script para la práctica 1
 echo Ejecutando el programa para 1000 elementos con cada método de ordenamiento
-cd /home/ivanovsky/Escritorio/Analisis-de-Algoritmos/Metodos-de-Ordenamiento
-./a.out < entradas
+for i in `seq 1 120`;
+do
+   ./op
+   ./a.out < entradas
+done
+./op
 echo Terminamos uwu
-
