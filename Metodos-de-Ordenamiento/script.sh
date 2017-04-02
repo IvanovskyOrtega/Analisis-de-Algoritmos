8a3509d153c73470173f77b018801294b09f4277
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-04-02 17:23:43 -0500

    Script  para pruebas de la práctica

--- Metodos-de-Ordenamiento/script.sh
+++ Metodos-de-Ordenamiento/script.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # Script para la práctica 1
-echo Ejecutando el programa para 1000 elementos con cada método de ordenamiento
+echo Ejecutando script...
 for i in `seq 1 120`;
 do
    ./op
    ./a.out < opciones.txt
 done
 ./op
-echo Terminamos uwu
+echo Terminamos 
