b38609f67ad6590d6c225d753c9494a44deeef03
Ivanovsky <ivanovskyortega@gmail.com>
2017-04-04 14:26:28 -0500

    Ya funciona bien el script

--- Metodos-de-Busqueda/script.sh
+++ Metodos-de-Busqueda/script.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 # Script para la práctica 2
 echo Ejecutando el script
-for i in `seq 1 1200`;
+for i in `seq 1 819`; #800 iteraciones para las busquedas lineal y binaria, 20 para la del arbol.
 do
-   ./op
-   ./a.out < opciones
+   ./a.out < opciones.txt #redirigir la entrada estándar, se mandan los argumentos desde el fichero opciones.txt
+   ./op #se actualiza el fichero opciones.txt
 done
-./op
 echo Terminamos
