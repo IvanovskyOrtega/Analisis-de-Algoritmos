34c04bb2ef40f27bd86ce6ced99188b7f43bfb1f
Ivanovsky <ivanovskyortega@gmail.com>
2017-04-22 13:34:45 -0500

    arbol.h

--- Metodos-de-Busqueda/arbol.h
+++ Metodos-de-Busqueda/arbol.h
@@ -11,13 +11,11 @@ struct nodo
 int
 aBB1 (int* A, int n, int num);
 int
-aBB2 ( int* A, int n, int num );
+aBB2 ( int* A, int n );
 void
 insertar (struct nodo **Raiz, int miDato1);
 struct nodo *
 nuevoNodo (int dato1);
-int *
-inordenSecuencial(struct nodo *Raiz, int* A);
 int
 buscarABB(struct nodo *Raiz, int num);
 #endif
