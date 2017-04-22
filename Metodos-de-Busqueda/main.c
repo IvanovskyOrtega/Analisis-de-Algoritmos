536471aae812b1a6250f86ed4d2599fffbe9f74f
Ivanovsky <ivanovskyortega@gmail.com>
2017-04-22 13:32:51 -0500

    Ya recibe argumentos

--- Metodos-de-Busqueda/main.c
+++ Metodos-de-Busqueda/main.c
@@ -3,8 +3,27 @@
 #include "busqueda.h"
 
 int
-main( void )
+main( int argc, char* argv[])
 {
-  busqueda();
+  if( argc < 4 )                  //Si faltan argumentos para ejecutar el programa automaticamente
+  {
+    printf("Argumentos insuficientes, se pedirán las entradas a continuacion\n");
+    printf("Pulsa Enter para continuar...\n");
+    getchar();
+    fflush(stdin);
+    obtenerEntradas();            //Se llama a la función obtenerEntradas()
+  }
+  if( argc == 4 )                 //Se ejecuta automaticamente con los argumentos recibidos
+  {
+    int n = atoi(argv[1]);        //Se convierten los argumentos a enteros
+    int op = atoi(argv[2]);
+    int num = atoi(argv[3]);
+    printf("Metodos:\n1 -- Busqueda Lineal\n2 -- Busqueda Binaria\n3 -- Arbol Binario de busqueda (Pruebas)");
+    printf("\n4 -- Arbol Binario de Busqueda (Normal)\n");
+    printf("\nSe ejecutará el programa con los siguientes argumentos:\n");
+    printf("\tMetodo: %d\n\tTamaño de arreglo: %d\n\tNumero a buscar: %d\n\n",op,n,num);
+    busqueda(n,op,num);           //Se llama a la función busuqeda()
+    printf("\n\nTermino la ejecucion\n");
+  }
   return 0;
 }
